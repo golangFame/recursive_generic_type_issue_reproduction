36de9cd35bce70e5cbb9503a7fae70d65fb10046
Hiro <laciferin@gmail.com>
2023-06-17 09:22:44 +0530

    added normal structs

--- minimum_repro/unstable_invalid_recursive_issue/seperated_files/file_2.go
+++ minimum_repro/unstable_invalid_recursive_issue/seperated_files/file_2.go
@@ -1,5 +1,12 @@
 package main
 
-//type T2[T any] struct {
-//	e *innerT[T, *T2[T]]
-//}
+/*type T2[T any] struct {
+	e *innerT[T, *T2[T]]
+}*/
+
+type H2 struct {
+	h *H2
+}
+type T2[T any] struct {
+	e *innerT[T, *T2[T]]
+}
--- minimum_repro/unstable_invalid_recursive_issue/seperated_files/main.go
+++ minimum_repro/unstable_invalid_recursive_issue/seperated_files/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 // It doesn't matter if the innerT struct unexported or exported, the result is the same.
 // It also doesn't matter if the R type parameter is infer to a pointer or not, the result is the same.
+
+// go: nosplit
 type innerT[T any, R *T1[T] | *T2[T]] struct {
 	Ref R
 }
@@ -12,10 +14,18 @@ type T1[T any] struct {
 	e *innerT[T, *T1[T]]
 }
 
-type T2[T any] struct {
-	e *innerT[T, *T2[T]]
+type innerH struct {
+	i *H1
+	y *H2
+}
+
+type H1 struct {
+	h1 *H1
 }
 
 func main() {
 	fmt.Println("didnt panic")
+
+	_ = innerH{}
+
 }
