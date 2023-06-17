64c0deaebcac7031e1c6cdf607ba4ce1b5390dbd
Hiro <laciferin@gmail.com>
2023-06-17 08:36:23 +0530

    fix deadlock_issue with generics

--- minimum_repro/deadlock_issue/with_generics/main.go
+++ minimum_repro/deadlock_issue/with_generics/main.go
@@ -2,10 +2,10 @@ package with_generics
 
 // It doesn't matter if the innerT1 struct unexported or exported, the result is the same.
 // It also doesn't matter if the R type parameter is infer to a pointer or not, the result is the same.
-type innerT1[T any, R T1[T]] struct {
-	reference *R
+type innerT1[T any, R *T1[T]] struct {
+	Ref *R
 }
 
 type T1[T any] struct {
-	e *innerT1[T, T1[T]]
+	T innerT1[T, *T1[T]]
 }
